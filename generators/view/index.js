1f53c59c6e5b01abad1b47e92eab75d54cad4383
thilo-behnke <janthilobehnke@gmx.de>
2018-05-14 11:14:47 +0200

    Added templating for libs and sapui5 comps

--- generators/view/index.js
+++ generators/view/index.js
@@ -1,5 +1,15 @@
 var Generator = require('yeoman-generator');
 
+const SAP_PATHS = {
+    MessageToast: 'sap/m/MessageToast',
+    MessageBox: 'sap/m/MessageBox'
+};
+
+const LIB_PATHS = {
+  momentjs: 'WebContent/lib/moment',
+  rxjs: 'WebContent/lib/rx.all'
+};
+
 module.exports = class extends Generator {
 
     prompting() {
@@ -12,10 +22,19 @@ module.exports = class extends Generator {
             name: 'path',
             message: 'Path to your view',
             default: ''
+        }, {
+            type: 'checkbox',
+            name: 'libraries',
+            message: 'Select the libraries you need',
+            choices: Object.keys(LIB_PATHS),
+            default: []
+        }, {
+            type: 'checkbox',
+            name: 'comps',
+            message: 'Select the SapUI5 components you need',
+            choices: Object.keys(SAP_PATHS),
+            default: []
         }]).then(answers => {
-            this.log('view name', answers.viewName);
-            this.log('path', answers.path);
-
             Object.assign(this, answers);
         });
     }
@@ -29,7 +48,12 @@ module.exports = class extends Generator {
         this.fs.copyTpl(
             this.templatePath('template.controller.js'),
             this.destinationPath(`WebContent/zss18_t1_web_frontend/${this.path}/${this.viewName}.controller.js`),
-            {viewName: this.viewName, path: `.${this.path}`}
+            {
+                viewName: this.viewName,
+                path: `.${this.path}`,
+                libraries: this.libraries.map(x => LIB_PATHS[x]),
+                comps: this.comps.map(x => SAP_PATHS[x])
+            }
         );
     }
 };
--- generators/view/templates/template.controller.js
+++ generators/view/templates/template.controller.js
@@ -1,44 +1,52 @@
+const libs = '<%= libraries %>'.split(',');
+const comps = '<%= comps %>'.split(',');
+
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function (Controller) {
-    "use strict";
+        'sap/ui/core/mvc/Controller',
+        ...comps,
+        ...libs,
+    ], (function () {
+        'use strict';
 
-    return Controller.extend('zss18_t1_web_frontend.<%= viewName %>', {
+        return function (Controller, ...[comps, libs]) {
 
-        /**
-         * Called when a controller is instantiated and its View controls (if available) are already created.
-         * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-         * @memberOf zss18_t1_web_frontend.<%= viewName %>
-         */
-        //	onInit: function() {
-        //
-        //	},
+            return Controller.extend('zss18_t1_web_frontend<%= path %>.<%= viewName %>', {
 
-        /**
-         * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-         * (NOT before the first rendering! onInit() is used for that one!).
-         * @memberOf zss18_t1_web_frontend.<%= viewName %>
-         */
-        //	onBeforeRendering: function() {
-        //
-        //	},
+                /**
+                 * Called when a controller is instantiated and its View controls (if available) are already created.
+                 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+                 * @memberOf zss18_t1_web_frontend<%= path %>.<%= viewName %>
+                 */
+                //	onInit: function() {
+                //
+                //	},
 
-        /**
-         * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-         * This hook is the same one that SAPUI5 controls get after being rendered.
-         * @memberOf zss18_t1_web_frontend.<%= viewName %>
-         */
-        //	onAfterRendering: function() {
-        //
-        //	},
+                /**
+                 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+                 * (NOT before the first rendering! onInit() is used for that one!).
+                 * @memberOf zss18_t1_web_frontend<%= path %>.<%= viewName %>
+                 */
+                //	onBeforeRendering: function() {
+                //
+                //	},
 
-        /**
-         * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-         * @memberOf zss18_t1_web_frontend.<%= viewName %>
-         */
-        //	onExit: function() {
-        //
-        //	}
+                /**
+                 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+                 * This hook is the same one that SAPUI5 controls get after being rendered.
+                 * @memberOf zss18_t1_web_frontend<%= path %>.<%= viewName %>
+                 */
+                //	onAfterRendering: function() {
+                //
+                //	},
 
-    });
-});
+                /**
+                 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+                 * @memberOf zss18_t1_web_frontend<%= path %>.<%= viewName %>
+                 */
+                //	onExit: function() {
+                //
+                //	}
+            });
+        }
+    })()
+);
