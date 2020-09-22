d278f1f06c4bbeff568407d2d01cbef581077714
Thilo Behnke <thilo.behnke@gmx.net>
2020-09-22 23:35:08 +0200

    make project name configurable, add readme

--- generators/view/index.js
+++ generators/view/index.js
@@ -17,42 +17,48 @@ const LIB_PATHS = {
 module.exports = class extends Generator {
 
     prompting() {
-        return this.prompt([{
-            type: 'input',
-            name: 'viewName',
-            message: 'Your view name'
-        }, {
-            type: 'input',
-            name: 'path',
-            message: 'Path to your view',
-            default: ''
-        }, {
-            type: 'checkbox',
-            name: 'libraries',
-            message: 'Select the libraries you need',
-            choices: Object.keys(LIB_PATHS),
-            default: []
-        }, {
-            type: 'checkbox',
-            name: 'comps',
-            message: 'Select the SapUI5 components you need',
-            choices: Object.keys(SAP_PATHS),
-            default: []
-        }]).then(answers => Object.assign(this, answers));
+        return this.prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: 'Your project name'
+            }, {
+                type: 'input',
+                name: 'viewName',
+                message: 'Your view name'
+            }, {
+                type: 'input',
+                name: 'path',
+                message: 'Path to your view',
+                default: ''
+            }, {
+                type: 'checkbox',
+                name: 'libraries',
+                message: 'Select the libraries you need',
+                choices: Object.keys(LIB_PATHS),
+                default: []
+            }, {
+                type: 'checkbox',
+                name: 'comps',
+                message: 'Select the SapUI5 components you need',
+                choices: Object.keys(SAP_PATHS),
+                default: []
+            }]).then(answers => Object.assign(this, answers));
     }
 
     writing() {
         this.fs.copyTpl(
             this.templatePath('template.view.xml'),
-            this.destinationPath(`WebContent/zss18_t1_web_frontend/${this.path}/${this.viewName}.view.xml`),
-            {viewName: this.viewName, path: `.${this.path}`}
+            this.destinationPath(`WebContent/${this.projectName}/${this.path}/${this.viewName}.view.xml`),
+            {projectName: this.projectName, viewName: this.viewName, path: `.${this.path}`}
         );
         this.fs.copyTpl(
             this.templatePath('template.controller.ejs'),
-            this.destinationPath(`WebContent/zss18_t1_web_frontend/${this.path}/${this.viewName}.controller.js`),
+            this.destinationPath(`WebContent/${this.projectName}/${this.path}/${this.viewName}.controller.js`),
             {
+                projectName: this.projectName,
                 viewName: this.viewName,
-                path: `.${this.path}`,
+                path: this.path ? `.${this.path}` : '',
                 libraries: this.libraries.map(x => LIB_PATHS[x]),
                 comps: this.comps.map(x => SAP_PATHS[x])
             }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Yeoman generator for sapui5 views
+
+Input for project name, view name and path to view.  
+Multi select for libs / sapui5 components.
+
+# How to use
+
+- In the root folder of this project: `npm link`
+- In the sapui5 project root: `yo sapui5:view`
